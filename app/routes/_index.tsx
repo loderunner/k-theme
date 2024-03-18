3f539077208a8166aef5adeb362edabc24ecbb64
Charles Francoise <charles.francoise@circleci.com>
2024-03-18 22:19:21 +0100

    try uploading to blob storage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -33,12 +33,27 @@ export async function action({ request }: ActionFunctionArgs) {
 
   const file = formData.get('image') as File;
   const id = nanoid(12);
+  const extension = file.type === 'image.jpeg' ? 'jpg' : 'png';
 
-  await put(id, file, {
+  console.log(`uploading ${file.name} (${file.size}B) as ${id}.${extension}`);
+  const blob = await put(`${id}.${extension}`, file, {
     addRandomSuffix: false,
     access: 'public',
     contentType: file.type,
   });
+
+  console.log(`finished uploading ${id}.${extension}: ${blob.url}`);
+
+  console.log(`uploading JSON entry as ${id}.json`);
+  await put(`${id}.json`, JSON.stringify({ url: blob.url }), {
+    addRandomSuffix: false,
+    access: 'public',
+    contentType: 'application/json',
+    multipart: true,
+  });
+
+  console.log(`finished uploading ${id}.json`);
+  console.log(`redirecting to /${id}`);
 
   return redirect(`/${id}`);
 }
