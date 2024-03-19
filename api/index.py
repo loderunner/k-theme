09b88814584c7d53aec1677fcaa15948f0b482d8
Charles Francoise <charles.francoise@circleci.com>
2024-03-19 18:36:09 +0100

    implement theme generation

--- api/index.py
+++ api/index.py
@@ -1,8 +1,65 @@
 from flask import Flask
+import requests
+import os, sys
+from PIL import Image
+from theme import generate_theme, rgb_to_css
+from werkzeug.exceptions import InternalServerError
+from dotenv import load_dotenv
+
+if os.environ.get("FLASK_ENV") is not None:
+    for env in [
+        "",
+        ".local",
+        f".{os.environ['FLASK_ENV']}",
+        f".{os.environ['FLASK_ENV']}.local",
+    ]:
+        load_dotenv(f".env{env}", override=True)
 
 app = Flask(__name__)
 
 
-@app.route("/api/message")
-def hello_world():
-    return "Hello, World!"
+@app.errorhandler(InternalServerError)
+def handle_error(err):
+    print(err, file=sys.stderr)
+
+
+@app.route("/api/theme/<theme_id>", methods=["GET"])
+def get_theme(theme_id):
+    json_url = f"{os.environ['BLOB_STORAGE_URL']}/{theme_id}.json"
+    print(f"retrieving {json_url}")
+    res = requests.get(json_url)
+    if res.ok == False:
+        return "entry not found", 404
+
+    print("retrieved entry, parsing JSON")
+    entry = res.json()
+
+    print(f"retrieving {entry['url']} and reading image")
+    res = requests.get(entry["url"], stream=True)
+    if res.ok == False:
+        return "image not found", 404
+
+    img = Image.open(res.raw).resize((256, 256), Image.Resampling.NEAREST)
+
+    print("generating theme")
+    theme = generate_theme(img)
+
+    print("theme generated")
+    return {
+        "black": rgb_to_css(theme[0]),
+        "red": rgb_to_css(theme[1]),
+        "green": rgb_to_css(theme[2]),
+        "yellow": rgb_to_css(theme[3]),
+        "blue": rgb_to_css(theme[4]),
+        "magenta": rgb_to_css(theme[5]),
+        "cyan": rgb_to_css(theme[6]),
+        "white": rgb_to_css(theme[7]),
+        "brightBlack": rgb_to_css(theme[8]),
+        "brightRed": rgb_to_css(theme[9]),
+        "brightGreen": rgb_to_css(theme[10]),
+        "brightYellow": rgb_to_css(theme[11]),
+        "brightBlue": rgb_to_css(theme[12]),
+        "brightMagenta": rgb_to_css(theme[13]),
+        "brightCyan": rgb_to_css(theme[14]),
+        "brightWhite": rgb_to_css(theme[15]),
+    }
---
+++ api/theme.py
@@ -0,0 +1,141 @@
+import numpy as np
+from matplotlib import colors
+
+
+def hsv_to_hsl(hsv):
+    """Converts pixels in HSV to HSL"""
+    h, sv, v = np.rollaxis(hsv, 1)
+    l = v * (1 - 0.5 * sv)
+    mask = (l != 0) & (l != 1)
+    sl = np.where(
+        mask,
+        np.divide((v - l), np.min((l, 1 - l), axis=0), where=mask),
+        0,
+    )
+    return np.stack((h, sl, l), axis=1)
+
+
+def rgb_to_hsl(rgb):
+    """Converts pixels in RGB to HSL"""
+    hsv = colors.rgb_to_hsv(rgb)
+    return hsv_to_hsl(hsv)
+
+
+def hsl_to_hsv(hsl):
+    """Converts pixels in HSL to HSV"""
+    h, sl, l = np.rollaxis(hsl, 1)
+    v = l + sl * np.min((l, 1 - l), axis=0)
+    mask = v != 0
+    s = np.where(mask, 2 * (1 - np.divide(l, v, where=mask)), 0)
+    return np.stack((h, s, v), axis=1)
+
+
+def hsl_to_rgb(hsl):
+    """Converts pixels in HSL to RGB"""
+    hsv = hsl_to_hsv(hsl)
+    return colors.hsv_to_rgb(hsv)
+
+
+def hsl_to_xyz(hsl):
+    """Converts pixels in HSL to XYZ"""
+    h, s, l = np.rollaxis(hsl, 1)
+    r = s * (1 - 2 * np.abs(l - 0.5))
+    x = r * np.cos(2 * np.pi * h)
+    y = r * np.sin(2 * np.pi * h)
+    z = l
+    return np.stack((x, y, z), axis=1)
+
+
+def xyz_to_hsl(xyz):
+    """Converts pixels in XYZ to HSL"""
+    x, y, z = np.rollaxis(xyz, 1)
+    h = np.fmod(np.arctan2(y, x) + 2 * np.pi, 2 * np.pi) / (2 * np.pi)
+    r = np.sqrt(x**2 + y**2)
+    l = z
+    mask = r != 0
+    s = np.where(mask, np.divide(r, 1 - 2 * np.abs(l - 0.5), where=mask), 0)
+    return np.stack((h, s, l), axis=1)
+
+
+def rgb_to_xyz(rgb):
+    """Converts pixels in RGB to XYZ"""
+    hsl = rgb_to_hsl(rgb)
+    return hsl_to_xyz(hsl)
+
+
+def xyz_to_rgb(xyz):
+    """Converts pixels in XYZ to RGB"""
+    hsl = xyz_to_hsl(xyz)
+    return hsl_to_rgb(hsl)
+
+
+def rgb_to_css(color):
+    return f"rgb({color[0]}, {color[1]}, {color[2]})"
+
+
+# base palette centroids in RGB 0..1
+base_centroids = np.array(
+    [
+        [0, 0, 0],
+        [0.5, 0, 0],
+        [0, 0.5, 0],
+        [0.5, 0.5, 0],
+        [0, 0, 0.5],
+        [0.5, 0, 0.5],
+        [0, 0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.33, 0.33, 0.33],
+        [1.0, 0, 0],
+        [0, 1.0, 0],
+        [1.0, 1.0, 0],
+        [0, 0, 1.0],
+        [1.0, 0, 1.0],
+        [0, 1.0, 1.0],
+        [1.0, 1.0, 1.0],
+    ]
+)
+
+
+def init_centroids():
+    """Returns a copy of base centroids in HSL-XYZ"""
+    return rgb_to_xyz(base_centroids)
+
+
+def snap_centroids(centroids, px):
+    """Returns new centroids snapped to the closest pixel value (Euclidean distance)"""
+    distances = np.apply_along_axis(
+        lambda c: np.linalg.norm(c - px, axis=1), 1, centroids
+    )
+    return px[np.argmin(distances, axis=1)]
+
+
+def assign_centroids(centroids, px):
+    """For each pixel in px, compute the closest centroid and return the list of assigned centroids"""
+    distances = np.apply_along_axis(
+        lambda p: np.linalg.norm(p - centroids, axis=1), 1, px
+    )
+    assigned = np.argmin(distances, axis=1)
+    return assigned
+
+
+def compute_means(assigned, px):
+    """For each group assigned to a centroid, compute the mean and return the list of means"""
+    return np.array(
+        [px[assigned == k].mean(axis=0) for k in range(len(base_centroids))]
+    )
+
+
+def generate_theme(img, max_iterations=10):
+    """Generate a theme from an image"""
+    rgb = np.array(img).reshape((img.size[0] * img.size[1], -1))[:, :3] / 255
+    px = rgb_to_xyz(rgb)
+    theme = init_centroids()
+    theme = snap_centroids(theme, px)
+    for i in range(max_iterations):
+        assigned = assign_centroids(theme, px)
+        means = compute_means(assigned, px)
+        snapped_means = snap_centroids(means, px)
+        if np.allclose(theme, snapped_means):
+            break
+        theme = snapped_means
+    return np.floor(xyz_to_rgb(theme) * 255).astype(int)
